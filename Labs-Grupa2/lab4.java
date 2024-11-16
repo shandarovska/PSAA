1e67ddaa568a677e5318b651d8b8ba466a50f101
Sandra <sandrashandarovska@gmail.com>
2024-11-17 00:38:29 +0100

    lab4

--- Labs-Grupa2/lab4.java
+++ Labs-Grupa2/lab4.java
@@ -1,8 +1,9 @@
-/*
-Да се напише функција која како аргумент го добива head јазолот на единечно поврзана листа и вредност N (цел број). 
-Функцијата треба да ја измине листата и да го отстрани N-тиот јазол од крајот на листата. Задачата да се реши и со класата SLinkedList
-и со класата LinkedList.
- */
+
+/*Да се напише функција која како аргумент го добива head јазолот на единечно поврзана листа и вредност N (цел број). 
+Функцијата треба да ја измине листата и да го отстрани N-тиот јазол од крајот на листата. Задачата да се реши 
+и со класата SLinkedList и со класата LinkedList. */
+import java.util.LinkedList;
+
 class Node<E> {
     protected E data;
     protected Node<E> next;
@@ -131,6 +132,7 @@ class SLinkedList<E> {
 public class lab4 {
 
     public static void main(String[] args) {
+        // SLinkedList
         SLinkedList lista = new SLinkedList();
 
         lista.insertLast(1);
@@ -140,10 +142,33 @@ public class lab4 {
         lista.insertLast(5);
         lista.insertLast(6);
         lista.insertLast(7);
-        // lista.insertLast(8);
         Node headd = new Node();
         headd = lista.getHead();
 
         lista.izbrishi(headd, 7);
+
+        // LinkedList
+        LinkedList<Integer> lista2 = new LinkedList();
+        lista2.add(1);
+        lista2.add(2);
+        lista2.add(3);
+        lista2.add(4);
+        lista2.add(5);
+        lista2.add(6);
+        lista2.add(7);
+        izbrishi(lista2, 6);
+    }
+
+    private static void izbrishi(LinkedList<Integer> lista, int i) {
+        if (i > lista.size())
+            return;
+        if (i == lista.size()) {
+            lista.removeFirst();
+            System.out.println(lista);
+            return;
+        }
+        int index = lista.size() - i;
+        lista.remove(index);
+        System.out.println(lista);
     }
 }
